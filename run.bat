441a4994021247a5a46e7e5753cb0225e380bdb9
Jônata <Jonatanc0511@gmail.com>
2021-12-29 09:57:38 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
 @echo off
 gcc -o rtype.exe -Iinclude -Llib src\rtype.c src\entity.c src\text_component.c lib\liballegro-5.0.10-monolith-mt.a
+pause
 
 copy lib\allegro-5.0.10-monolith-mt.dll
-pause
 .\rtype.exe
 del allegro-5.0.10-monolith-mt.dll
 del rtype.exe
